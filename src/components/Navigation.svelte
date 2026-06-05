fff8095a46c010021138acc3415af08b24cdd313
Alex Bustos <bustosalex1@gmail.com>
2026-06-04 20:40:38 -0400

    add new navigation on mobile

--- src/components/Navigation.svelte
+++ src/components/Navigation.svelte
@@ -4,6 +4,7 @@
     import ThemeToggle from "@components/ThemeToggle.svelte";
 
     import { fade, slide } from "svelte/transition";
+    import { backIn, backOut } from "svelte/easing";
     import { MediaQuery } from "svelte/reactivity";
 
     const md = new MediaQuery("min-width: 768px");
@@ -52,7 +53,10 @@ slugs can be added in the `links` variable. This component also displays the
 title of the website (my name).
 -->
 <div
-    class="flex flex-row w-full h-11 border-line border items-stretch bg-secondary-bg hard-shadow"
+    class={[
+        "flex flex-row w-full h-11 border-line border items-stretch bg-secondary-bg relative z-30",
+        { "hard-shadow": !show },
+    ]}
 >
     <a
         href="/"
@@ -83,10 +87,7 @@ title of the website (my name).
 
     <ThemeToggle />
     <button
-        class={[
-            "md:hidden content-center w-11 hover:text-primary group transition-all duration-150 hover:bg-primary/10 cursor-pointer border-line border-l",
-            { "z-50 border-none": show },
-        ]}
+        class="md:hidden content-center w-11 hover:text-primary group transition-all duration-150 hover:bg-primary/10 cursor-pointer border-line border-l"
         onclick={() => {
             show = !show;
         }}
@@ -101,41 +102,38 @@ title of the website (my name).
             />
         {/if}
     </button>
+
+    {#if show}
+        <div
+            class="absolute top-full -left-px -right-px bg-secondary-bg border border-line flex flex-col items-stretch"
+            in:slide={{ axis: "y", duration: 250, easing: backOut }}
+            out:slide={{ axis: "y", duration: 250, easing: backIn }}
+        >
+            {#each links as { title, link, base }, i (link)}
+                <a
+                    href={link}
+                    class={[
+                        {
+                            "underline text-primary":
+                                (url.includes(base ?? link) && link !== "/") ||
+                                (url.includes("posts") && link === "/") ||
+                                (url === "/" && link === "/") ||
+                                (url === "" && link === "/"),
+                        },
+                        "hover:text-primary hover:bg-primary/10 transition-all duration-150 px-4 border-line content-center whitespace-nowrap h-11 font-mono text-sm",
+                        { "border-b": i < links.length - 1 },
+                    ]}
+                >
+                    {title}
+                </a>
+            {/each}
+        </div>
+    {/if}
 </div>
 
 {#if show}
-    <div
-        class="fixed left-0 top-0 h-full bg-bg border-r border-line z-50 flex flex-col items-stretch min-w-60"
-        transition:slide={{ axis: "x", duration: 150 }}
-    >
-        <div class="p-2 border-b border-line bg-primary">
-            <a
-                href="/"
-                class="font-bold whitespace-nowrap transition-all duration-150 content-center font-mono text-white"
-            >
-                Alex Bustos
-            </a>
-        </div>
-        {#each links as { title, link, base } (link)}
-            <a
-                href={link}
-                class={[
-                    {
-                        "underline text-primary":
-                            (url.includes(base ?? link) && link !== "/") ||
-                            (url.includes("posts") && link === "/") ||
-                            (url === "/" && link === "/") ||
-                            (url === "" && link === "/"),
-                    },
-                    "hover:text-primary transition-all duration-300 pr-4 pl-6 border-line content-center whitespace-nowrap border-b py-2 font-mono text-sm",
-                ]}
-            >
-                {title}
-            </a>
-        {/each}
-    </div>
     <button
-        class="inset-0 z-40 fixed backdrop-blur-lg"
+        class="fixed inset-0 z-20 backdrop-blur-lg"
         aria-label="menu-backdrop"
         transition:fade={{ duration: 150 }}
         onclick={() => {
